9c7e4833665ac8d69b0cdd25fafad0a472a9eee2
wanghaozhen <haozhenwang297@gmail.com>
2024-09-16 17:44:23 +0800

    Finish JavaFX tutorial

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 public class Duke {
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
